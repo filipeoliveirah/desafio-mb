682c47df4e0dfd40a9519fa792368cf296766c1e
Filipe <filipeoliveirah@gmail.com>
2025-03-19 21:39:41 -0300

    feat: implement home page

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,7 @@ import HomePage from '@/components/organisms/HomePage.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-  </header>
-
   <main>
     <HomePage />
   </main>
 </template>
-
