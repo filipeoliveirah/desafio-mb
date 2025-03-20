c0111507f9bf471e6bf26a284488f460019003f6
Filipe <filipeoliveirah@gmail.com>
2025-03-19 21:39:29 -0300

    feat: create Home page

---
+++ src/components/organisms/HomePage.vue
@@ -0,0 +1,20 @@
+<script setup>
+import FormStep from '@/components/molecules/FormSteps.vue'
+</script>
+
+<template>
+  <main class="home">
+    <FormStep />
+  </main>
+</template>
+
+<style scoped lang="scss">
+.home {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  height: 100vh;
+  background-color: black;
+}
+</style>
