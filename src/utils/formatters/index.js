8baa50c9c8fee3ef16aebdde62898749d92b4bc9
Filipe <filipeoliveirah@gmail.com>
2025-03-20 11:09:42 -0300

    chore: import formatters to index.js

--- src/utils/formatters/index.js
+++ src/utils/formatters/index.js
@@ -1,12 +1,19 @@
 import { formatCNPJ } from './cnpj'
 import { isValidCPF, formatCPF } from './cpf'
-import { formatPhone } from './phone'
+import { formatPhone, isValidPhone } from './phone'
 import { isValidEmail } from './email'
+import { isAtLeast18YearsOld, isValidDate } from './date'
 
 export {
   formatCNPJ,
+
   isValidCPF,
   formatCPF,
+
+  isValidPhone,
   formatPhone,
-  isValidEmail
+
+  isValidEmail,
+  isValidDate,
+  isAtLeast18YearsOld
 }
